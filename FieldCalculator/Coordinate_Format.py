f3c8a62ae1a0af93ef7ac2a9a9401cb214d1160a
brennejm <44592903+brennejm@users.noreply.github.com>
2018-10-30 10:50:51 -0400

    Create Coordinate_Format.py
    Given two variables (longitude, latitude), it will round the coordinates and set the number of decimal places equal to numDec. It will also check to see if any of the ending digits rounded to zero and will pad the coordinates until they equal numDec.

---
+++ FieldCalculator/Coordinate_Format.py
@@ -0,0 +1,22 @@
+## Program: Coordinate Format
+## Description: Given two variables (longitude, latitude), it
+## will round the coordinates and set the number of decimal
+## places equal to numDec.
+
+def coordinates(lon,lat):
+    # Set numDec equal to how many decimal places you want.
+    numDec = 4
+
+    x = str(round(float(lon),numDec))
+    y = str(round(float(lat),numDec))
+
+    # If any ending digits rounded to zero, pad the coordinate to equal numDec.
+    if len(x[x.find('.')+1:]) < numDec:
+        while len(x[x.find('.')+1:]) < numDec:
+            x = x + '0'
+
+    if len(y[y.find('.')+1:]) < numDec:
+        while len(y[y.find('.')+1:]) < numDec:
+            y = y + '0'
+            
+    return '{}, {}'.format(x,y)
