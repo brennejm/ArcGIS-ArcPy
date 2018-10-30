3c3ffcf931084b67adae1ec696a5e28850b1a402
brennejm <44592903+brennejm@users.noreply.github.com>
2018-10-30 11:25:21 -0400

    Update Coordinate_Format.py

--- FieldCalculator/Coordinate_Format.py
+++ FieldCalculator/Coordinate_Format.py
@@ -1,22 +1,20 @@
-## Program: Coordinate Format
+## Program: Coordinate Creator
 ## Description: Given two variables (longitude, latitude), it
 ## will round the coordinates, set the number of decimal
 ## places equal to numDec, and return a string.
 
-def coordinates(lon,lat):
-    # Set numDec equal to how many decimal places you want.
-    numDec = 4
-
-    x = str(round(float(lon),numDec))
-    y = str(round(float(lat),numDec))
+def coordinates(dec,lon,lat):
+    
+    x = str(round(float(lon),dec))
+    y = str(round(float(lat),dec))
 
     # If any ending digits rounded to zero, pad the coordinate to equal numDec.
-    if len(x[x.find('.')+1:]) < numDec:
-        while len(x[x.find('.')+1:]) < numDec:
+    if len(x[x.find('.')+1:]) < dec:
+        while len(x[x.find('.')+1:]) < dec:
             x = x + '0'
 
-    if len(y[y.find('.')+1:]) < numDec:
-        while len(y[y.find('.')+1:]) < numDec:
+    if len(y[y.find('.')+1:]) < dec:
+        while len(y[y.find('.')+1:]) < dec:
             y = y + '0'
             
     return '{}, {}'.format(x,y)
